be4d497296a41b39e51233ee6dd4fb307b507e2c
Travis Gorman <thefarbetween@me.com>
2017-02-20 01:17:55 -0600

    classes

--- scripts/components/Order.js
+++ scripts/components/Order.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import CSSTransitionGroup from 'react-addons-css-transition-group'
 import h from '../helpers'
+import autobind from 'autobind-decorator'
 
-const Order = React.createClass({
-	propTypes: {
-		fishes: React.PropTypes.object,
-		order: React.PropTypes.object,
-		removeFromOrder: React.PropTypes.func,
-	},
+@autobind
+class Order extends React.Component {
 	renderOrder(key) {
 		let fish = this.props.fishes[key];
 		let count = this.props.order[key];
@@ -30,10 +27,10 @@ const Order = React.createClass({
 		  	<span className="price">{h.formatPrice(count * fish.price)}</span>
 		  </li>
 	  )
-	},
-	render() {
+	}
+  render() {
 		let orderIds = Object.keys(this.props.order);
-		let total = orderIds.reduce((prevTotal, key)=> {
+		let total = orderIds.reduce((prevTotal, key) =>{
 			let fish = this.props.fishes[key];
 			let count = this.props.order[key];
 			let isAvailable = fish && fish.status === 'available';
@@ -42,9 +39,9 @@ const Order = React.createClass({
 			}
 			return prevTotal;
 		}, 0);
-		return (
-		  <div className="order-wrap">
-		  	<h2 className="order-title">Your Order</h2>		  	
+    return (  
+      <div className="order-wrap">
+				<h2 className="order-title">Your Order</h2>		  	
 		  	<CSSTransitionGroup 
 		  		className="order" 
 		  		component="ul"
@@ -56,11 +53,74 @@ const Order = React.createClass({
 			  		<strong>Total:</strong>
 			  		{h.formatPrice(total)}
 			  	</li>
-		  	</CSSTransitionGroup>
+		  	</CSSTransitionGroup>      
+      </div>
+    );
+  }
+}
 
-		  </div>
-		)
-	}
-});
+Order.PropTypes = {
+	fishes: 			   React.PropTypes.object,
+	order: 					 React.PropTypes.object,
+	removeFromOrder: React.PropTypes.func,
+}
 
 export default Order
+
+
+// const Order = React.createClass({
+
+// 	renderOrder(key) {
+// 		let fish = this.props.fishes[key];
+// 		let count = this.props.order[key];
+// 		let removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}>&times;</button>
+// 		if (!fish) {
+// 			return <li key={key}>Sorry, fish no longer available!{removeButton}</li>;
+// 		}
+// 		return (
+// 		  <li key={key}>
+// 			  <span>
+// 				  <CSSTransitionGroup 
+// 				  	component="span" 
+// 				  	transitionName="count"
+// 				  	transitionLeaveTimeout={250}
+// 				  	transitionEnterTimeout={250}>
+// 				  	<span key={count}>{count}</span>
+// 				  </CSSTransitionGroup>
+// 				  	lbs {fish.name} {removeButton}		  
+// 			  </span>
+// 		  	<span className="price">{h.formatPrice(count * fish.price)}</span>
+// 		  </li>
+// 	  )
+// 	},
+// 	render() {
+// 		let orderIds = Object.keys(this.props.order);
+// 		let total = orderIds.reduce((prevTotal, key)=> {
+// 			let fish = this.props.fishes[key];
+// 			let count = this.props.order[key];
+// 			let isAvailable = fish && fish.status === 'available';
+// 			if (fish && isAvailable) {
+// 				return prevTotal + (count * parseInt(fish.price) || 0);
+// 			}
+// 			return prevTotal;
+// 		}, 0);
+// 		return (
+// 		  <div className="order-wrap">
+// 		  	<h2 className="order-title">Your Order</h2>		  	
+// 		  	<CSSTransitionGroup 
+// 		  		className="order" 
+// 		  		component="ul"
+// 		  		transitionName="order"
+// 		  		transitionEnterTimeout={500}
+// 		  		transitionLeaveTimeout={500}>
+// 		  		{orderIds.map(this.renderOrder)}
+// 			  	<li className="total">
+// 			  		<strong>Total:</strong>
+// 			  		{h.formatPrice(total)}
+// 			  	</li>
+// 		  	</CSSTransitionGroup>
+
+// 		  </div>
+// 		)
+// 	}
+// });
